32f755c982559e3d9e538467aa5216424bf80c61
AestheticAaditya0203 <aadityajoshi@appinessworld.com>
2023-07-28 13:17:40 +0530

    hhey there

--- app/create-post/page.jsx
+++ app/create-post/page.jsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePost = () => {
